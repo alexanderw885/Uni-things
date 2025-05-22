8c22715f64575dfda03987a2fa38c859ee5bdf18
Alex <alexanderw@uvic.ca>
2025-05-21 17:40:30 -0700

    cleaning up repo

--- CSC360/CSC360A1/blank.c
+++
@@ -1,8 +0,0 @@
-
-// Tester function for Pman.c
-// Never uses sleep, so can test utime/stime
-//also never prints to console, so not annoying
-int main()
-{
-	for(;;){}
-}
--- CSC360/CSC360A3/reset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm *.IMA
-cp Backup/*.IMA ./
